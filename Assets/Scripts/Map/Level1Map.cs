f1b4168e79cf70d4b17512b342580555f64c66b5
imjae <code@kakao.com>
2021-09-29 23:39:46 +0900

    포털에서 던져지는 애니메이션 슬라임 버전 추가

--- Assets/Scripts/Map/Level1Map.cs
+++ Assets/Scripts/Map/Level1Map.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Level1Map : MonoBehaviour
 {
-    public GameObject slime;
     public Transform respawnPoint;
 
     public GameObject parent;
@@ -20,14 +19,7 @@ public class Level1Map : MonoBehaviour
     [SerializeField]
     private DragonMonsterFactory dragonFactory = null;
 
-    public Vector3[] wayPoint = new Vector3[]{
-        new Vector3(45f, -0.5f, -26.89f),
-        new Vector3(45f, -0.5f, -56.89f),
-        new Vector3(-75f, -0.5f, -56.89f),
-        new Vector3(-75f, -0.5f, 3.1f),
-        new Vector3(45f, -0.5f, -26.89f)
-    };
-
+    public GameObject[] wayPoint;
 
     void Start()
     {
