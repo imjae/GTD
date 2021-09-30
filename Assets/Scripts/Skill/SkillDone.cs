bb8b54bf6d9c50512b416f31d21d807aeed108d2
BigBear621 <rabbitbear621@gmail.com>
2021-09-30 13:21:36 +0900

    gg

--- Assets/Scripts/Skill/SkillDone.cs
+++ Assets/Scripts/Skill/SkillDone.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class SkillDone : MonoBehaviour
 {
     public float duration;
+    float durationTime;
 
     void Start()
     {
+        durationTime = Time.deltaTime + duration;
         StartCoroutine(Duration());
     }
 
     IEnumerator Duration()
     {
         Debug.Log("지속시간: " + duration);
-        yield return new WaitForSeconds(duration);
-        Destroy(gameObject);
+        while (true)
+        {
+            gameObject.transform.localScale /= (Time.deltaTime / durationTime);
+            if (durationTime <= Time.deltaTime)
+                Destroy(gameObject);
+            yield return null;
+        }
     }
 }
