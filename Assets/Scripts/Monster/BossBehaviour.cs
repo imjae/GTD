52eff7087e3587f51f04c448683fdb72f60b5c75
imjae <code@kakao.com>
2021-10-03 15:13:24 +0900

    보스 8자 움직임 구현

--- Assets/Scripts/Monster/BossBehaviour.cs
+++ Assets/Scripts/Monster/BossBehaviour.cs
@@ -7,8 +7,17 @@ public class BossBehaviour : MonoBehaviour
     public GameObject fireBreath;
     // 보스 출연 연출 끝남을 알리는 플래그
     public bool isFInishBossMovie = false;
+    // 보스가 움직여야하는 플래그
+    public bool isMoving = false;
     // 보스가 공격할 타겟 표시 이펙트
     public GameObject targetEffect;
+    // 보스 이동 경로 포인트
+    public GameObject bossWaypoint1;
+    public GameObject bossWaypoint2;
+    public GameObject bossWaypoint3;
+    public GameObject bossWaypoint4;
+    // 보스가 어느 웨이포인트 향하고 있는지 담고있는 변수
+    public string currentLookAtWaypoint = "";
 
 
     private GameObject level1Map;
@@ -27,6 +36,7 @@ public class BossBehaviour : MonoBehaviour
         fireBreath.SetActive(false);
         bossHealthBar = GameObject.FindWithTag("MainCanvas").transform.Find("BossHealthBar").gameObject;
         StartCoroutine(AttackTower());
+        StartCoroutine(MovePattern());
     }
 
     // Update is called once per frame
@@ -41,13 +51,33 @@ public class BossBehaviour : MonoBehaviour
         {
             transform.Translate(Vector3.forward * 0.15f);
         }
+
+        if(isMoving)
+        {
+            if (currentLookAtWaypoint.Equals("bossWaypoint1"))
+            {
+                transform.position = Vector3.Slerp(transform.position, bossWaypoint1.transform.position, Time.deltaTime);
+            }
+            else if(currentLookAtWaypoint.Equals("bossWaypoint2"))
+            {
+                transform.position = Vector3.Slerp(transform.position, bossWaypoint2.transform.position, Time.deltaTime);
+            }
+            else if (currentLookAtWaypoint.Equals("bossWaypoint3"))
+            {
+                transform.position = Vector3.Slerp(transform.position, bossWaypoint3.transform.position, Time.deltaTime);
+            }
+            else if (currentLookAtWaypoint.Equals("bossWaypoint4"))
+            {
+                transform.position = Vector3.Slerp(transform.position, bossWaypoint4.transform.position, Time.deltaTime);
+            }
+        }
     }
 
     public IEnumerator AttackTower()
     {
         while (true)
         {
-            yield return new WaitForSeconds(10f);
+            yield return new WaitForSeconds(15f);
             GameManager.Instance.RefreshBuiledTowerArr();
             GameObject[] currentBuildedTowerArr = GameManager.Instance.GetBuiledTowerArr();
             Debug.Log(currentBuildedTowerArr.Length);
@@ -65,6 +95,48 @@ public class BossBehaviour : MonoBehaviour
                     Instantiate(targetEffect, new Vector3(targetPosition.x, targetPosition.y + 5f, targetPosition.z), Quaternion.Euler(-90f, 0f, 0f));
                 }
             }
+        }
+    }
+
+    public IEnumerator MovePattern()
+    {
+        //1 : 271.95f,  1008.9f,    -848.1f
+        //2 : 316.6f,   1005.3f,    -893.19f
+        //3 : 316.6f,   1004.7f,    -813.6f
+        //4 : 375.7f,   1009.8f,    -848.1f
+        while (true)
+        {
+            currentLookAtWaypoint = "bossWaypoint1";
+            transform.LookAt(bossWaypoint1.transform);
+            yield return new WaitForSeconds(8f);
+
+            isMoving = true;
+            yield return new WaitForSeconds(3f);
+            isMoving = false;
+
+            currentLookAtWaypoint = "bossWaypoint2";
+            transform.LookAt(bossWaypoint2.transform);
+            yield return new WaitForSeconds(5f);
+
+            isMoving = true;
+            yield return new WaitForSeconds(3f);
+            isMoving = false;
+
+            currentLookAtWaypoint = "bossWaypoint3";
+            transform.LookAt(bossWaypoint3.transform);
+            yield return new WaitForSeconds(5f);
+            
+            isMoving = true;
+            yield return new WaitForSeconds(3f);
+            isMoving = false;
+
+            currentLookAtWaypoint = "bossWaypoint4";
+            transform.LookAt(bossWaypoint4.transform);
+            yield return new WaitForSeconds(5f);
+
+            isMoving = true;
+            yield return new WaitForSeconds(3f);
+            isMoving = false;
         }
     }
 
